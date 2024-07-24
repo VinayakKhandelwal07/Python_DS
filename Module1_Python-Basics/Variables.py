7117b74c5e973e21a3e9d279de20114ef675eb17
VinayakKhandelwal <139864778+VinayakKhandelwal07@users.noreply.github.com>
2024-07-24 15:18:29 +0530

    Create Variables.py

---
+++ Module1_Python-Basics/Variables.py
@@ -0,0 +1,27 @@
+# Creating a variable
+x = 12
+y = "VInayak"
+
+print(x)
+print(y)
+
+# Casting (If you want to specify the data type of a variable)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+# Get the Type 
+x = 12
+y = "VInayak"
+
+print(type(x)) # x wll be int type
+print(type(y)) # y will be string type
+
+# VARIABLE NAME 
+
+#A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
