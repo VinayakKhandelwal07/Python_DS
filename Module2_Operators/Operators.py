f63e6a3e4543bf56393c4c122a37867ce4f2a62b
VinayakKhandelwal <139864778+VinayakKhandelwal07@users.noreply.github.com>
2024-07-24 15:46:02 +0530

    Create Operators.py

---
+++ Module2_Operators/Operators.py
@@ -0,0 +1,34 @@
+# Python divides the operators in the following groups:
+
+# Arithmetic operators
+# Assignment operators
+# Comparison operators(==,!=,< ,> >=,<=)
+# Logical operators(and,or,not)
+# Identity operators(Is ,Is Not)
+# Membership operators(In ,Not in)
+# Bitwise operators(&,|,^,<<,>>,~)
+
+1 . Arithmetic Operators
+         Operator   	 Name    	    Example	  
+          +	       Addition	        x + y	
+          -	       Subtraction 	    x - y	
+          *	       Multiplication	  x * y	
+          /        Division	        x / y	
+          %	       Modulus	          x % y	(#Shows Remainder)
+          **	     Exponentiation	  x ** y	(#Same as x*x*x*..(y Times) 
+          //	     Floor division	    x // y(#the floor division // rounds the result down to the nearest whole number)
+2. Python Assignment Operators
+          Operator	      Example	    Same As
+           =               x = 5       x = 5	
+          += 	            x+= 3	      x = x + 3	
+          -=	            x -= 3	    x = x - 3	
+          *=	            x *= 3	    x = x * 3	
+          /=	            x /= 3	    x = x / 3	
+          %=	            x %= 3	    x = x % 3	
+          //=	            x //= 3	    x = x // 3	
+          **=	            x **= 3	    x = x ** 3	
+         &=	              x &= 3	    x = x & 3	
+        |=	              x |= 3	    x = x | 3	
+        ^=                x^= 3	      x = x ^ 3	
+       >>=	              x >>= 3	    x = x >> 3	
+        <<=	              x<<= 3	    x = x << 3	
