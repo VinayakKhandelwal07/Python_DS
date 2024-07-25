b63978e533a087382e70320fcb9bfc61015a1aea
Vinayak Khandelwal <139864778+VinayakKhandelwal07@users.noreply.github.com>
2024-07-25 12:31:20 +0530

    Create tuple.py

---
+++ Module4_Tuple/tuple.py
@@ -0,0 +1,17 @@
+#Tuples are used to store multiple items in a single variable.
+
+# Create a tuple
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+# Access tuple items
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])#(print the second item in the tuple)
+#Negative Indexing
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1]#(print the last item of the tuple)
+
+#Range of tuple
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])#(Return the third, fourth, and fifth item)
+      
